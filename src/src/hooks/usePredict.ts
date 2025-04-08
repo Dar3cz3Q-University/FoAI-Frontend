71cdd6c32da56e54e8af5367d58ea5b08f846d8f
Dariusz Homa <dariuszhoma7@gmail.com>
2025-04-08 20:22:30 +0200

    fix: temporary log url

--- src/src/hooks/usePredict.ts
+++ src/src/hooks/usePredict.ts
@@ -27,6 +27,7 @@ export const usePredict = () => {
     files.forEach((file) => formData.append("files", file));
 
     try {
+      console.log(API_URL);
       const response = await fetch(`${API_URL}/predict`, {
         method: "POST",
         body: formData,
